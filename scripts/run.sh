8f1d87f63c65aa3f8ab37d86379a7c5d09ce83e9
hitesh22rana <hitesh22rana@gmail.com>
2024-04-17 16:24:07 +0530

    chore(fix): fixed bugs and dependencies error

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,12 @@
 # !/bin/bash
 
-# check if .env file exists, if not exit
-if [ ! -f .env ]; then
+# check if .env file exists in backend and frontend folders
+if [ ! -f backend/.env ]; then
+    echo "No .env file found in backend folder, please create one, you can use the .env.example file as a template, exiting..."
+    exit
+fi
+
+if [ ! -f frontend/.env ]; then
     echo "No .env file found, please create one, you can use the .env.example file as a template, exiting..."
     exit
 fi
